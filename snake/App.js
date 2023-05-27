945f25a2775d4b7201d163b81988ad43a86fbe89
lucasafonsobastos <lucasafonsob@hotmail.com>
2023-05-26 21:52:21 -0300

    atualiando snake

--- snake/App.js
+++ snake/App.js
@@ -1,20 +1,163 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { GameEngine } from "react-native-game-engine";
+//import { TouchableOpacity } from "react-native-gesture-handler";
+
+
+import Food from "./src/components/Food";
+import Head from "./src/components/Head";
+import Tail from "./src/components/Tail";
+import Constants from "./src/js/Constants";
+import GameLoop from "./src/js/GameLoop";
+
 
 export default function App() {
+  const BoardSize = Constants.GRID_SIZE * Constants.CELL_SIZE;
+  const engine = useRef(null);
+  const [isGameRunning, setIsGameRunning] = useState(true);
+  const randomPositions = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+  const resetGame = () => {
+    engine.current.swap({
+      head: {
+        position: [0, 0],
+        size: Constants.CELL_SIZE,
+        updateFrequency: 10,
+        nextMove: 10,
+        xspeed: 0,
+        yspeed: 0,
+        renderer: <Head />,
+      },
+      food: {
+        position: [
+          randomPositions(0, Constants.GRID_SIZE - 1),
+          randomPositions(0, Constants.GRID_SIZE - 1),
+        ],
+        size: Constants.CELL_SIZE,
+        updateFrequency: 10,
+        nextMove: 10,
+        xspeed: 0,
+        yspeed: 0,
+        renderer: <Food />,
+      },
+      tail: {
+        size: Constants.CELL_SIZE,
+        elements: [],
+        renderer: <Tail />,
+      },
+    });
+    setIsGameRunning(true);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.canvas}>
+      <GameEngine
+        ref={engine}
+        style={{
+          width: BoardSize,
+          height: BoardSize,
+          flex: null,
+          backgroundColor: "white",
+        }}
+        entities={{
+          head: {
+            position: [0, 0],
+            size: Constants.CELL_SIZE,
+            updateFrequency: 10,
+            nextMove: 10,
+            xspeed: 0,
+            yspeed: 0,
+            renderer: <Head />,
+          },
+          food: {
+            position: [
+              randomPositions(0, Constants.GRID_SIZE - 1),
+              randomPositions(0, Constants.GRID_SIZE - 1),
+            ],
+            size: Constants.CELL_SIZE,
+            renderer: <Food />,
+          },
+          tail: {
+            size: Constants.CELL_SIZE,
+            elements: [],
+            renderer: <Tail />,
+          },
+        }}
+        systems={[GameLoop]}
+        running={isGameRunning}
+        onEvent={(e) => {
+          switch (e) {
+            case "game-over":
+              alert("Game over!");
+              setIsGameRunning(false);
+              return;
+          }
+        }}
+      />
+      <View style={styles.controlContainer}>
+        <View style={styles.controllerRow}>
+          <TouchableOpacity onPress={() => engine.current.dispatch("move-up")}>
+            <View style={styles.controlBtn} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.controllerRow}>
+          <TouchableOpacity
+            onPress={() => engine.current.dispatch("move-left")}
+          >
+            <View style={styles.controlBtn} />
+          </TouchableOpacity>
+          <View style={[styles.controlBtn, { backgroundColor: null }]} />
+          <TouchableOpacity
+            onPress={() => engine.current.dispatch("move-right")}
+          >
+            <View style={styles.controlBtn} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.controllerRow}>
+          <TouchableOpacity
+            onPress={() => engine.current.dispatch("move-down")}
+          >
+            <View style={styles.controlBtn} />
+          </TouchableOpacity>
+        </View>
+      </View>
+      {!isGameRunning && (
+        <TouchableOpacity onPress={resetGame}>
+          <Text
+            style={{
+              color: "white",
+              marginTop: 15,
+              fontSize: 22,
+              padding: 10,
+              backgroundColor: "grey",
+              borderRadius: 10
+            }}
+          >
+            Start New Game
+          </Text>
+        </TouchableOpacity>
+      )}
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
+  canvas: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  controlContainer: {
+    marginTop: 10,
+  },
+  controllerRow: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  controlBtn: {
+    backgroundColor: "yellow",
+    width: 100,
+    height: 100,
   },
 });
